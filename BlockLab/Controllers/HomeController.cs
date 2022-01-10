6402fe2466dd70199ffdf81e94bccc4ee7c1d8c7
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2022-01-10 20:58:43 +0400

    base app infrastructure

---
+++ BlockLab/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlockLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Error(string id)
+        {
+            switch (id)
+            {
+                default: return Content($"Status --- {id}");
+                case "404": return View("Error404");
+            }
+        }
+    }
+}
