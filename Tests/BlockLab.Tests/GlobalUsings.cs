8b59fb2c0b60b1abaac28de3863ba24822676ae4
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2022-01-12 12:37:24 +0400

    Первый тест сервиса

---
+++ Tests/BlockLab.Tests/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using BlockLab.Services;
+global using BlockLab.WebModels;
+global using BlockLab.Dal.Data;
+global using Moq;
---
+++ Tests/BlockLab.Tests/Services/ResearchInfoServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlockLab.Tests.Services;
+
+[TestClass]
+public class ResearchInfoServiceTests
+{
+    private BlockLabContext _context;
+
+    [TestInitialize]
+    public void Init()
+    {
+        var contextOptions = new DbContextOptionsBuilder<BlockLabContext>()
+            .UseInMemoryDatabase(nameof(ResearchInfoServiceTests))
+            .Options;
+        _context = new BlockLabContext(contextOptions);
+    }
+
+    [TestMethod]
+    public void GetTop10Researches_Call_ShouldType()
+    {
+        var service = new ResearchInfoService(_context);
+
+        var actual = service.GetTop10Researches().Result;
+
+        Assert
+            .IsInstanceOfType(actual, typeof(IEnumerable<ResearchWebModel>));
+    }
+
+}
+
