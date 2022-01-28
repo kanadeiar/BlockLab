ba6b77ff0d723800f97376c06839e3079f3a7498
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2022-01-28 10:37:12 +0400

    Подправка тестов

--- Tests/BlockLab.Tests/Services/ResearchInfoPagiServiceTests.cs
+++ Tests/BlockLab.Tests/Services/ResearchInfoPagiServiceTests.cs
@@ -18,24 +18,36 @@ public class ResearchInfoPagiServiceTests
             Text = "Test",
             TypeResearch = new TypeResearch { Name = "Тест", },
             ResearchObject = new ResearchObject { Name = "Тест", },
-            UserId = "test",
+            UserId = "test0",
             WorkShift = new WorkShift { Name = "Тест", },
         });
         _context.SaveChanges();
     }
 
-    //[TestMethod]
-    //public void GetPagiFilterSortResearches_Call_ShouldType()
-    //{
-    //    var filter = new ResearchFilter();
-    //    var service = new ResearchInfoPagiService(_context);
+    [TestMethod]
+    public void GetPagiFilterSortResearches_Call_ShouldType()
+    {
+        var userStoreStub = Mock.Of<IUserStore<User>>();
+        var userManagerMock = new Mock<UserManager<User>>(userStoreStub, null, null, null, null, null, null, null, null);
+        var user = new User 
+        { 
+            Id = "test0", 
+            SurName = "Testov",
+            FirstName = "Test",
+            Patronymic = "Testovich",
+            UserName = "Test", 
+            Email = "test@example.com" 
+        };
+        userManagerMock.Setup(_ => _.FindByIdAsync("test0")).Returns(Task.FromResult(user));
+        var filter = new ResearchFilter();
+        var service = new ResearchInfoPagiService(_context, userManagerMock.Object);
 
-    //    var actual = service.GetPagiFilterSortResearches(filter).Result;
+        var actual = service.GetPagiFilterSortResearches(filter).Result;
 
-    //    Assert
-    //        .IsInstanceOfType(actual, typeof(ResearchPagiWebModel));
-    //    Assert
-    //        .AreEqual(1, actual.Researches.Count());
-    //}
+        Assert
+            .IsInstanceOfType(actual, typeof(ResearchPagiWebModel));
+        Assert
+            .AreEqual(1, actual.Researches.Count());
+    }
 }
 
